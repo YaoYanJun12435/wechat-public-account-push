9b99696f76baddbfd2f66a31c9eef7ef21836919
YaoYanJun12435 <44473548+YaoYanJun12435@users.noreply.github.com>
2022-09-27 23:10:41 +0800

    修改模板

--- config/index.cjs
+++ config/index.cjs
@@ -63,7 +63,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '姚燕军',
+      name: '马雪林',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oYosX6rhqAYjknm_COtte1SdmVas',
       // 你想对他发送的模板消息的模板ID
