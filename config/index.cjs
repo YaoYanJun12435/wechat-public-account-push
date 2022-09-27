cc5cf4cc34e43a974771c681add658324b902748
YaoYanJun12435 <44473548+YaoYanJun12435@users.noreply.github.com>
2022-09-27 22:39:22 +0800

    appsecret error fix

--- config/index.cjs
+++ config/index.cjs
@@ -12,7 +12,7 @@ const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '1b29ae7466af2ba77bb007b35057cab',
+  APP_SECRET: 'e1b29ae7466af2ba77bb007b35057cab',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
